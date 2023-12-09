12f71f6cace806ca7189c43dcd68f6c0bdc66521
Bharatan Dileep <bharathandileep01@gmail.com>
2023-12-09 11:52:21 +0530

    php array and array method

--- Array/associateArray.php
+++ Array/associateArray.php
@@ -27,8 +27,35 @@ echo $person["city"]; // Outputs: New York
 foreach ($person as $key => $value) {
     echo "$key: $value <br>";
 }
-
-
-
+// ARRAY METHOD:
+// length of an array is determain : sizeof($arr) or count(arr)
+echo sizeof($numbers)
+// search in an array  - in_arrya() and array_search()
+//                     - in_array($element,$arr,true) - check weather the element is persent or not return 0 or 1 
+//                     - array_serach($element,$arr,true)) - return the index or key 
+echo in_array(10,$numbers,true) // the true is optional for strict mode it check the datatype of element by defualt flase
+echo in_array(10,$numbers) //return the index
+//The array_column() function returns the values from a single column in the input array.
+$a = array(
+    array(
+      'id' => 5698,
+      'first_name' => 'Peter',
+      'last_name' => 'Griffin',
+    ),
+    array(
+      'id' => 4767,
+      'first_name' => 'Ben',
+      'last_name' => 'Smith',
+    ),
+    array(
+      'id' => 3809,
+      'first_name' => 'Joe',
+      'last_name' => 'Doe',
+    )
+  );
+  
+  $last_names = array_column($a, 'last_name');
+  print_r($last_names);
+// refer other method from - https://www.w3schools.com/php/php_ref_array.asp
 ?>
 
--- Array/indexArry.php
+++ Array/indexArry.php
@@ -29,7 +29,31 @@
 foreach($numbers as $value){
     echo "$value , <br>"
 }
+// ARRAY METHOD:
+// length of an array is determain : sizeof($arr) or count(arr)
+echo sizeof($numbers)
+// search in an array  - in_arrya() and array_search()
+//                     - in_array($element,$arr,true) - check weather the element is persent or not return 0 or 1 
+//                     - array_serach($element,$arr,true)) - return the index or key 
+echo in_array(10,$numbers,true) // the true is optional for strict mode it check the datatype of element by defualt flase
+echo in_array(10,$numbers) //return the index
+print_r(array_pop($numbers))//The array_pop() function deletes the last element of an array.
+print_r(array_push($numbers , 4)) //insert an element to the last of the array
+print_r(sort($numbers)) //sort an array in acending order
+print_r(rsort($numbers)) //sort an array in decending order
 
+list(a,b,c) = $numbers //-list(vaariables) which store the elements of array in varable (eg like deepstructuring in js)
+$num = range(0,5) //- creating an array with element range 0-4
+print_r($num)
+//array_filter($array, "callback/condition")
+
+function filter($value){
+    return($value > 2)
+}
+
+print_r(array_filter($numbers,"filter")) 
+
+// refer other method from - https://www.w3schools.com/php/php_ref_array.asp
 
 
 ?>
