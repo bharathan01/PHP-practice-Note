61c974d2e89d4ba4fe8d3b0e63458ad0acc11fb9
bharathan01 <bharathandileep01@gmail.com>
2023-12-12 11:49:22 +0530

    include and require method text file add

---
+++ includeAndRequire.php/main.php
@@ -0,0 +1,18 @@
+<!-- Common header included -->
+<?php include 'header.php'; ?>
+
+<!-- Main module content goes here -->
+<div class="w-100 bg-primary" style="height:624px;">
+     <div class="container text-light text-center">
+        <h1>Main Content</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores ipsa ipsam soluta commodi saepe consectetur, 
+            quas rerum dolorum recusandae omnis delectus ut sequi cum neque modi quaerat blanditiis deleniti qui, 
+            sit inventore at id natus. Doloremque in cumque, repellat expedita sequi saepe, aspernatur eos architecto 
+            perferendis iure aliquam possimus natus eveniet ratione ipsum? Rerum perspiciatis, fugiat quisquam amet 
+            officiis odit, eum deserunt illo quaerat facere esse vero adipisci? Voluptas velit mollitia ut eaque. 
+            Consequuntur repellat veniam, nisi dolorem tenetur quam quibusdam provident porro nihil. 
+            Velit repellat excepturi fugit laudantium similique iste. Architecto sit itaque accusamus dolore amet, corporis error laborum!</p> 
+    </div>
+</div>
+
+<?php require 'footer.php'; ?>
