a2e5cca80585b3e3ff709b1b5088bb35512b8d1e
bharathan01 <bharathandileep01@gmail.com>
2023-12-21 15:45:12 +0530

    form validate

--- formValidation.php
+++ formValidation.php
@@ -5,22 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<?php 
-
-  if($_SERVER["REQUEST_METHOD"] == 'POST'){
-    if(isset($_POST['submit'])){
-      $name = $_REQUEST['name'];
-      $email = $_REQUEST['email'];
-      $password = $_REQUEST['password'];
-      $terms = $_REQUEST['terms'];
-      echo $name, $password, $email,$terms;
-    }
-     
-  }
-
-?>
-
-
 
 <style>
   form{
@@ -34,7 +18,7 @@
     padding: 10px;
     margin: 10px 0 10px 0px;
   }
-  input[type='button']{
+  input[type='submit']{
     width: 100px;
     height: 40px;
     background-color: black;
@@ -45,6 +29,46 @@
   }
 
 </style>
+
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+  $error = [];
+        if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+          if(empty($_POST['name'])){
+            $error['nameEty'] = "Name is required";
+          }
+          if(empty($_POST['email'])){
+            $error['emailEty'] = "Name is required";
+          }
+          elseif(filter_var('email',FILTER_VALIDATE_EMAIL)){
+            $error['notEmail'] = "PEmail is required";
+          }
+          if(empty($_POST['password'])){
+            $error['passEty'] = "password is required";
+          }
+          if(empty($_POST['terms'])){
+            $error['checkTerms'] = "terams and conditions is required";
+          }
+        }
+         else {
+            $error['meassege'] = "Invalid form - some fields are empty";
+        }
+
+
+        if(count($error) === 0){
+          $name = trim($_POST['name']) && filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+          $email = isset($_POST['email']) ? (trim($_POST['email']) && filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL)) : '';
+          $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+          $name = isset($_POST['terms']);
+            echo "Name: " . $name . "<br>";
+            echo "Email: " . $email . "<br>";
+            echo "Password: " . $password . "<br>";
+            echo "Terms: " . $terms . "<br>"; 
+        }
+}
+?>
 <body>
     <section style="width: 100%;
     height: 100vh;
@@ -57,14 +81,20 @@
         justify-content: center;">
             <form method="POST" action="<?php  echo $_SERVER['PHP_SELF'] ?>" >
                 <label for="Name">Name</label>
-                <input name="name" type="text" id="name" placeholder="Name"/>
+                <input name="name" type="text" id="name" placeholder="Name">
+                <p style="color: red;"><?php echo isset($error['nameEty']) ? $error['nameEty']: ''; ?></p>
                 <label for="Email">Email</label>
-                <input name="email" type="email" id="Email" placeholder="Email"/>
+                <input name="email" type="email" id="Email" placeholder="Email">
+                <p style="color: red;"><?php echo isset($error['emailEty']) ? $error['emailEty']: ''; ?></p>
+                <p style="color: red;"><?php echo isset($error['notEmail']) ? $error['notEmail']: ''; ?></p>
                 <label for="password">password</label>
-                <input name="password" id="password" type="password" placeholder="password"/>
+                <input name="password" id="password" type="password" placeholder="password">
+                <p style="color: red;"><?php echo isset($error['passEty']) ? $error['passEty']: ''; ?></p>
                 <label for="terms">Privacy&policy</label>
                 <input name="terms" type="checkbox" id="terms"/><br>
-                <input type="button" name="submit" value="submit" /> 
+                <p style="color: red;"><?php echo isset($error['checkTerms']) ? $error['checkTerms']: ''; ?></p>
+                <input type="submit" name="submit" value="submit" > 
+                <p style="color: red;"><?php echo isset($error['meassege']) ? $error['meassege']: ''; ?></p>
             </form>
         </div>
     </section>
